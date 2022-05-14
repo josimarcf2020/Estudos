4eaca2939abdd41b2a55beacd37a6a0cefd3d82d
Josimar da Cunha Ferreira <62252790+josimarcf2020@users.noreply.github.com>
2022-05-14 07:56:55 -0300

    Iniciando front end

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document
+    .querySelector("button.fat")
+    .addEventListener("click", function(){
+    
+        document
+           
+            .querySelector("#footer")
+            .classList
+            .toggle("hide")
+    }
+)
